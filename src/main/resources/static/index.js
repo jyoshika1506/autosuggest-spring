8f3253545dd04188118ea338d90d7e70b2ee75d9
Jyoshika <jyoshika1506@gmail.com>
2026-07-18 12:54:39 +0530

    random user implementation

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,46 @@
 // alert("Hello from JS");
 console.log("Hello from JS");
+
+var users = [
+    {
+        "name" : "Jane Smith",
+        "gender" : "Female",
+        "img" : "jane.png"
+    },
+    {
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
+    }
+]
+
+var id = 0;
+function toggleUser() {
+
+    id = (id + 1)%2
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = users[id].gender;
+}
+function randomUser() {
+    fetch('https://randomuser.me/api/')
+    .then(function(response) {
+        return response.json();
+    } )
+    .then(function(data){
+        var userData = data.results[0];
+        
+    var userImage = document.getElementById("user-image");
+    userImage.src = userData.picture.large;
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = userData.name.first + " " + userData.name.last;
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = userData.gender;
+})
+.catch(function(err) {
+    console.log(err);
+});
+}
